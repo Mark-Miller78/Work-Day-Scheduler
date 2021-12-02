8b6b35b18569995d10033047bfd919c376e1c14f
Mark Miller <markmiller7858@gmail.com>
2021-12-01 19:09:03 -0500

    hour background changes with hour

--- Assets/javascript.js
+++ Assets/javascript.js
@@ -4,6 +4,32 @@ var containerEl = document.getElementById("schedule")
 
 $(currentDayEl).text(now);
 
-console.log(moment().hours());
 
 
+var hourTracker = function(){
+    var rightNow = moment().hour();
+
+    $(".time-block").each(function(){
+        var scheduleHour = parseInt($(this).attr("id"));
+        
+        if(scheduleHour < rightNow){
+            $(this)
+            .addClass("past")
+            .removeClass("present")
+            .removeClass("future");
+
+        } else if(scheduleHour == rightNow){
+            $(this)
+            .addClass("present")
+            .removeClass("future")
+            .removeClass("past");
+
+        } else{
+            $(this)
+            .addClass("future")
+            .removeClass("present")
+            .removeClass("past");
+        }
+    });
+};
+hourTracker();
